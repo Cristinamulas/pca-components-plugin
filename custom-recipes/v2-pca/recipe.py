339962a61705ff9de7c07bc73f723b40c8182bda
admin <admin>
2024-01-13 09:01:22 -0400

    Edited files

--- custom-recipes/v2-pca/recipe.py
+++ custom-recipes/v2-pca/recipe.py
@@ -22,9 +22,13 @@ from dataiku.customrecipe import get_recipe_config
 # Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
 
 # To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_A_names = get_input_names_for_role('input_A_role')
+input_A_names = get_input_names_for_role('input')
 # The dataset objects themselves can then be created like this:
 input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
+
+# For outputs, the process is the same:
+output_A_names = get_output_names_for_role('main_output')
+output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
 
 # For outputs, the process is the same:
 output_A_names = get_output_names_for_role('main_output')
